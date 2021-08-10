7b2101697f90f2642c2cb2cac9127610c4c23b3b
Sami Virpioja <sami.virpioja@utopiaanalytics.com>
2021-08-10 14:36:05 +0300

    Add support to fasttext for language detection (#20)
    Co-authored-by: Kirian Guiller <kirianguiller@gmail.com>

--- opusfilter/filters.py
+++ opusfilter/filters.py
@@ -5,11 +5,13 @@ import string
 import math
 import difflib
 import itertools
+from typing import Iterator, List, Tuple
 
 import regex
 from langid.langid import LanguageIdentifier, model
 import pycld2
 from bs4 import BeautifulSoup as bs
+import fasttext
 
 from . import FilterABC, ConfigurationError
 from .lm import CrossEntropyFilter, CrossEntropyDifferenceFilter  # noqa: F401
@@ -109,7 +111,8 @@ class AverageWordLengthFilter(FilterABC):
         self.pass_empty = pass_empty
         super().__init__(**kwargs)
 
-    def _average_word_len(self, sentence):
+    @staticmethod
+    def _average_word_len(sentence):
         parts = sentence.split()
         if parts:
             return len(''.join(parts)) / len(parts)
@@ -182,16 +185,25 @@ class CharacterScoreFilter(FilterABC):
 class LanguageIDFilter(FilterABC):
     """Language identification confidence filter"""
 
-    def __init__(self, languages=None, id_method='langid', thresholds=None, **kwargs):
+    def __init__(self, languages=None, id_method='langid', thresholds=None,
+                 fasttext_model_path=None, **kwargs):
         if languages is None:
             raise ConfigurationError("A list of language codes needs to be defined")
+        if id_method == 'fasttext' and not fasttext_model_path:
+            raise ConfigurationError("FastText language ID method was choosen without specifying "
+                                     "any path to fasttext model")
+        if id_method != 'fasttext' and fasttext_model_path:
+            raise ConfigurationError("FastText language ID method was not choosen but fasttext "
+                                     "path to model was set")
+        self.fasttext_model = fasttext.load_model(fasttext_model_path) \
+            if id_method == 'fasttext' else None
         self.languages = languages
         self.id_method = id_method
         self.thresholds = [0] * len(self.languages) if thresholds is None else thresholds
         self.identifier = LanguageIdentifier.from_modelstring(model, norm_probs=True)
         super().__init__(**kwargs)
 
-    def confidence(self, sentence, lan):
+    def confidence(self, sentence: str, lan: str) -> float:
         """Return confidence of the identifier"""
         if not sentence:
             # Prevent filtering empty lines
@@ -209,7 +221,7 @@ class LanguageIDFilter(FilterABC):
                 cldconf = 0.0
             return cldconf
 
-        elif self.id_method == 'langid':
+        if self.id_method == 'langid':
             try:
                 lidetails = self.identifier.classify(sentence)
             except Exception:
@@ -219,12 +231,28 @@ class LanguageIDFilter(FilterABC):
                 liconf = 0.0
             return liconf
 
-    def score(self, pairs):
+        if self.id_method == 'fasttext':
+            lang, confidence = self._fasttext_predict_lang(sentence)
+            if lang != lan:
+                liconf = 0.0
+            else:
+                liconf = confidence
+            return liconf
+
+        raise ValueError("Unknown language identification method '%s'" % self.id_method)
+
+    def score(self, pairs: List[Tuple[str, str]]) -> Iterator[List[float]]:
         for pair in pairs:
             yield [self.confidence(sent, self.languages[idx]) for idx, sent in enumerate(pair)]
 
-    def accept(self, score):
+    def accept(self, score: Tuple[float, float]) -> bool:
         return all(conf > threshold for conf, threshold in zip(score, self.thresholds))
+
+    def _fasttext_predict_lang(self, texts: List[str]) -> Tuple[str, float]:
+        output = self.fasttext_model.predict(texts, k=1)
+        confidence = output[1][0]
+        label = output[0][0][9:]
+        return label, confidence
 
 
 class TerminalPunctuationFilter(FilterABC):
--- tests/test_filters.py
+++ tests/test_filters.py
@@ -1,7 +1,12 @@
-import copy
+import os
+import requests
+import shutil
+import tempfile
 import unittest
 
+from opusfilter import ConfigurationError
 from opusfilter.filters import *
+from opusfilter.util import file_download
 
 
 class TestLongestCommonSubstringFilter(unittest.TestCase):
@@ -10,7 +15,7 @@ class TestLongestCommonSubstringFilter(unittest.TestCase):
                  ('abcd', 'bc'), ('abcd', 'ab abcd cd'), ('abcd ', ' abcd'), ('ab cd', 'a bc d')]
     tri_inputs = [('abcd', 'abcd', 'abcd'), ('abcd', 'abcd', 'efgh'), ('abcd', '', ''), ('', '', ''),
                   ('abcd', 'abc', 'bc'), ('abcd', 'xbcd', 'xabx')]
-    
+
     def test_bilingual(self):
         testfilter = LongestCommonSubstringFilter(threshold=0.8, require_all=True)
         expected = [([1], False), ([0], True), ([0.5], True), ([0], True), ([0], True),
@@ -35,3 +40,59 @@ class TestLongestCommonSubstringFilter(unittest.TestCase):
         for result, correct in zip(results, expected):
             self.assertSequenceEqual(result, correct)
 
+
+class TestFasttext(unittest.TestCase):
+
+    fasttext_inputs = ["This sentence is in english", "Je suis une phrase en français"]
+
+    pairs_inputs = [
+        ("This sentence is in english", "Je suis une phrase en français"),
+        ("me llamo bernardo", "je m'appelle Bernard")
+    ]
+
+    model_url = 'https://dl.fbaipublicfiles.com/fasttext/supervised-models/lid.176.ftz'
+
+    @classmethod
+    def setUpClass(self):
+        self.tempdir = tempfile.mkdtemp()
+        self.testmodel = os.path.join(self.tempdir, 'model.ftz')
+        try:
+            file_download(self.model_url, self.testmodel)
+        except requests.exceptions.ConnectionError:
+            self.testmodel = None
+
+    @classmethod
+    def tearDownClass(self):
+        shutil.rmtree(self.tempdir)
+
+    def test_missing_model(self):
+        with self.assertRaises(ConfigurationError):
+            model = LanguageIDFilter(
+                languages=['en', 'fr'], id_method='fasttext', thresholds=[0.8, 0.99])
+
+    def test_wrong_method_with_model(self):
+        with self.assertRaises(ConfigurationError):
+            model = LanguageIDFilter(
+                languages=['en', 'fr'], thresholds=[0.8, 0.99], fasttext_model_path=self.tempdir)
+
+    def test_fasttext_predict_lang(self):
+        if self.testmodel is None:
+            self.skipTest("Failed to download test resources")
+        model = LanguageIDFilter(
+            languages=['en', 'fr'], id_method='fasttext', thresholds=[0.8, 0.99],
+            fasttext_model_path=self.testmodel)
+        expected = ['en', 'fr']
+        results = [model._fasttext_predict_lang(fasttext_input)[0]
+                   for fasttext_input in self.fasttext_inputs]
+        self.assertSequenceEqual(expected, results)
+
+    def test_fasttext_accept(self):
+        if self.testmodel is None:
+            self.skipTest("Failed to download test resources")
+        model = LanguageIDFilter(
+            languages=['en', 'fr'], id_method='fasttext', thresholds=[0.8, 0.99],
+            fasttext_model_path=self.testmodel)
+        pair_scores = model.score(self.pairs_inputs)
+        pair_expecteds = [True, False]
+        for pair_score, pair_expected in zip(pair_scores, pair_expecteds):
+            self.assertEqual(model.accept(pair_score), pair_expected)
