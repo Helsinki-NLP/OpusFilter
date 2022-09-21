e8e67db1aa9204794ae844829bf65f30ea5a2ba8
svirpioj <sami.virpioja@helsinki.fi>
2022-09-21 16:43:17 +0000

    Deploying to gh-pages from  @ 542fcde3061dadc3caf6a4fab8451878c25c2571 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+g616df62',
+    VERSION: '0.1.dev1+g542fcde',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
