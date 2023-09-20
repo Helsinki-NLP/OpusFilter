7c3898b8f65cdcd086b287140afddb1118bc9534
svirpioj <sami.virpioja@helsinki.fi>
2023-09-20 18:44:55 +0000

    Deploying to gh-pages from @ Helsinki-NLP/OpusFilter@f379f122a918c0e3e60334a4c100bb32a4711d77 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.0.0rc1.post1',
+    VERSION: '3.0.0rc2',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
