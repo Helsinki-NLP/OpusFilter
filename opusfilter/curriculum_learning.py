65b1d7612e1b6207d7fd88e2699c794bb5facaf4
miau1 <aulamo.mikko@gmail.com>
2024-10-21 11:34:14 +0300

    add geometric mean

--- opusfilter/curriculum_learning.py
+++ opusfilter/curriculum_learning.py
@@ -2,6 +2,7 @@
 
 import logging
 from collections import Counter
+from statistics import geometric_mean
 
 from sklearn import  preprocessing
 from sklearn.cluster import KMeans
@@ -22,7 +23,7 @@ class BabyStep:
 
     """
 
-    def __init__(self, sample_score_file, data_size, k=10, data_inc=100000, output_file=None, workdir=None, chunksize=500000):
+    def __init__(self, sample_score_file, data_size, k=10, data_inc=100000, gmean=False, output_file=None, workdir=None, chunksize=500000):
         self.df = load_dataframe(sample_score_file)
         self.k = k + int((data_size - data_inc)/data_inc)
         self.output_file = output_file
@@ -45,8 +46,16 @@ class BabyStep:
 
         # Low values are clean, high values are noisy
         adjusted_centers = self.kmeans.cluster_centers_ * self.direction_vector
+    
+        print(gmean)
+        if gmean:
+            temp_centers = adjusted_centers + abs(adjusted_centers.min()) + 0.01
+            means = [geometric_mean(m) for m in temp_centers]
+        else:
+            means = np.mean(adjusted_centers, axis=1)
+
         # The buckets are labeled from cleanest to noisiest with labels from 0 to k 
-        self.clean_order = [np.where(np.argsort(np.mean(adjusted_centers, axis=1))==i)[0][0] for i in range(self.k)]
+        self.clean_order = [np.where(np.argsort(means)==i)[0][0] for i in range(self.k)]
 
     @property
     def direction_vector(self):
