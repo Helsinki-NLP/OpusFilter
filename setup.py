cb87e21bb00ad2602deb04222b185ca0a7606b6f
Sami Virpioja <sami.virpioja@helsinki.fi>
2026-05-20 14:55:56 +0300

    cleanup curriculum code

--- setup.py
+++ setup.py
@@ -62,10 +62,6 @@ heliport_require = [
     'heliport>=0.10.0'
 ]
 
-curriculum_require = [
-    'k-means-constrained'
-]
-
 tests_require = [
     'pytest'
 ]
@@ -78,7 +74,7 @@ docs_require = [
 ]
 
 all_require = eflomal_require + jieba_require + mecab_require + laser_require + varikn_require + \
-    curriculum_require + tests_require + docs_require
+    tests_require + docs_require
 
 setuptools.setup(
     name="opusfilter",
@@ -99,7 +95,6 @@ setuptools.setup(
     extras_require={'test': tests_require, 'pycld2': pycld2_require, 'fasttext': fasttext_require,
                     'eflomal': eflomal_require, 'jieba': jieba_require, 'mecab': mecab_require,
                     'laser': laser_require, 'varikn': varikn_require, 'heliport': heliport_require,
-                    'curriculum': curriculum_require,
                     'docs': docs_require, 'all': all_require},
     classifiers=(
         "Programming Language :: Python :: 3",
